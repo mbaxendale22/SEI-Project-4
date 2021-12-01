62a34030124d2346457425ca168ed18a68c4b816
Matthew Baxendale <mdbaxendale1@gmail.com>
2021-12-01 15:23:51 +0000

    CRUD routes added for household app

--- household/views.py
+++ household/views.py
@@ -16,12 +16,43 @@ class HouseholdIndexView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         return Response(serialized_hh.data, status=status.HTTP_200_OK)
 
-
-
+    def post(self, request, pk):
+        try:
+            household = HouseholdSerializer(data=request.data)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        if household.is_valid():
+            household.save()
+            return Response(household.data, status=status.HTTP_201_CREATED)
+        else: 
+            return Response(household.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 
 class HouseholdDetailView(APIView):
+
+    def delete(self, request, pk):
+        try:
+            household = House.objects.get(id=pk)
+            house.delete()
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk):
+        try:
+            household = Household.objects.get(id=pk)
+            updated_household = HouseholdSerializer(household, data=request.data)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        if updated_household.is_valid():
+            updated_household.save()
+            return Response(updated_household.data, status=status.HTTP_202_ACCEPTED)
+        else: 
+            return Response(updated_household.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+
     def get(self, request, pk):
         try:
             household = Household.objects.get(id=pk)
