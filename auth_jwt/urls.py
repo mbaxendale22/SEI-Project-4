3db7e4d51252b3b636f75574395b16af2f8e0346
Matthew Baxendale <mdbaxendale1@gmail.com>
2021-12-04 18:10:36 +0000

    login & registration, config & tested

--- auth_jwt/urls.py
+++ auth_jwt/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import UserIndexView, UserDetailView
+from .views import UserIndexView, UserDetailView, RegisterView, LoginView
 
 urlpatterns = [
 
     path('<int:pk>/', UserDetailView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
     path('', UserIndexView.as_view()),
     ]
--- auth_jwt/views.py
+++ auth_jwt/views.py
@@ -4,6 +4,13 @@ from rest_framework.response import Response
 from rest_framework import status 
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from django.contrib.auth import get_user_model
+from django.conf import settings
+import jwt
+from .serializers import UserSerializer
+User = get_user_model()
 
 
 class UserIndexView(APIView):
@@ -59,3 +66,36 @@ class UserDetailView(APIView):
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
         return Response(serialized_user.data, status=status.HTTP_200_OK)
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Registration successful'})
+
+        return Response(serializer.errors, status=422)
+
+
+class LoginView(APIView):
+
+    def get_user(self, email):
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise PermissionDenied({'message': 'Invalid credentials'})
+
+    def post(self, request):
+        print("********** hitting the correct view")
+        email = request.data.get('email')
+        password = request.data.get('password')
+        print(password)
+
+        user = self.get_user(email)
+        print(user)
+        if not user.check_password(password):
+            raise PermissionDenied({'message': 'Invalid credentials'})
+
+        token = jwt.encode({'sub': user.id}, settings.SECRET_KEY, algorithm='HS256')
+        return Response({'token': token, 'message': f'Welcome back {user.username}!'})
