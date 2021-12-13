7592fc033a7ca46eea357c0308bbfa64bfa8460d
Matthew Baxendale <mdbaxendale1@gmail.com>
2021-12-13 21:58:32 +0000

    deployment

--- server/project/settings.py
+++ server/project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@3932gk3al7g8jcnr$$0h+npeax*jj!-emql1r8i8yl(_d7t+z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
